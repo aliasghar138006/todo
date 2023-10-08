678a8f76dc3511fb33a5098179fb0d59a6b446b7
aliasghar138006 <aliasgharshahneh1380@gmail.com>
2023-10-08 17:48:10 +0330

    up

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,35 +4,33 @@ import TodoUser from "../../../model/Users";
 import { VerifyPassword } from "../../../utils/auth";
 import Connect from "../../../utils/connectD";
 
-
 const authOptions = {
-    session : {strategy : 'jwt'},
-    providers : [
-        CredentialsProvider(
-            {
-                async authorize(credentials , req){
-                    const {email , password} = credentials;
-                    if(!email , !password){
-                        throw Error("invalid data!")
-                    };
+  session: { strategy: "jwt" },
+  secret: process.env.SECRET,
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+        if ((!email, !password)) {
+          throw Error("invalid data!");
+        }
 
-                    await Connect();
+        await Connect();
 
-                    const user = await TodoUser.findOne({email : email});
-                    if(!user) {
-                        throw Error("user not found!")
-                    }
+        const user = await TodoUser.findOne({ email: email });
+        if (!user) {
+          throw Error("user not found!");
+        }
 
-                    const verifyPassword = await VerifyPassword(password , user.password);
-                    if(!verifyPassword){
-                        throw Error("password not correct!")
-                    }
+        const verifyPassword = await VerifyPassword(password, user.password);
+        if (!verifyPassword) {
+          throw Error("password not correct!");
+        }
 
-                    return {email};
-                }
-            }
-        )
-    ]
-}
+        return { email };
+      },
+    }),
+  ],
+};
 
 export default NextAuth(authOptions);
